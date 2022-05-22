f17398cf1d8f42d4686841bd1c0b0cd802c86ebe
Natalia <tonio1811@gmail.com>
2022-05-22 09:38:13 +0200

    реализация кнопок слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let sliderToggle = document.querySelector('.slider__toggle');
+let priceCurrent = document.querySelector('.price__table');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +12,18 @@ navToggle.addEventListener('click', function () {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+sliderToggle.addEventListener('click', function() {
+  if (sliderToggle.classList.contains('slider__toggle--current-01')) {
+    priceCurrent.className('price__table');
+    priceCurrent.classList.add('price__table--current-01');
+  } else if (sliderToggle.classList.contains('slider__toggle--current-02')) {
+    priceCurrent.className('price__table');
+    priceCurrent.classList.add('price__table--current-02');
+  } else if (sliderToggle.classList.contains('slider__toggle--current-03')) {
+    priceCurrent.className('price__table');
+    priceCurrent.classList.add('price__table--current-03');
   }
 });
